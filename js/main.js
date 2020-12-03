3a65c2f4ed4a4df91213dc0595e8b412379849c0
dmsco19@naver.com <69883408+singsong19@users.noreply.github.com>
2020-12-03 22:26:02 +0900

    HTML: Retouch

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ document.addEventListener('scroll', () => { //스크롤이 될 때마다 얼마�
     if(window.screenY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
-        navbar.classList.remove('navbar--dark')
+        navbar.classList.remove('navbar--dark');
     }
 });
 
 //scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu')
+const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     
     const target = event.target;
@@ -21,18 +21,18 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null){
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link)
 });
 
 //Navbar toggle button
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn').
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', ()=> {
     navbarMenu.classList.toggle('open');
-})
+});
 
 // Contact Me button scrolling
-const homeContactBtn = document.querySelector('.home__contact')
+const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
